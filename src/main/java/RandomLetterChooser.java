76ed3cd134c95f5e40a3d60eed99e44a58b77061
notauhuynh <142616016+notauhuynh@users.noreply.github.com>
2024-04-22 10:39:51 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -3,10 +3,7 @@ public class RandomLetterChooser extends RandomStringChooser
 	private ArrayList<String>letters;
 	public RandomLetterChooser(String str)
 	{ 
-		letters = new ArrayList<Strings>();
-		for(int i = 0; i < str.length - 1; i++){
-		  letters.add(str.substring(i, i + 1));
-		}
+		super(getSingleLetters(str));
 	}
 	
 	
